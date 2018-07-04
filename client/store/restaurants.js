674893eeb43aad5878e0720b503de38ed1e397db
Sameer Khan <sameer_khan@student.uml.edu>
2018-07-04 11:10:57 -0400

    Added basic restaurants store

---
+++ client/store/restaurants.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+  list: [],
+});
+
+export const mutations = {
+  add(state, restaurant) {
+    state.list.push(restaurant);
+  },
+  emptyList(state) {
+    state.list = [];
+  },
+};
+
+export const getters = {
+  list: state => state.list,
+};
---
+++ server/api/restaurants/controllers/Restaurants.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Restaurants.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Restaurants`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve restaurants records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    return strapi.services.restaurants.fetchAll(ctx.query);
+  },
+
+  /**
+   * Retrieve a restaurants record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+      return ctx.notFound();
+    }
+
+    return strapi.services.restaurants.fetch(ctx.params);
+  },
+
+  /**
+   * Count restaurants records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.restaurants.count(ctx.query);
+  },
+
+  /**
+   * Create a/an restaurants record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.restaurants.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an restaurants record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.restaurants.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an restaurants record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.restaurants.remove(ctx.params);
+  }
+};
