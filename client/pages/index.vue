c44596541713d7e7afc0045f723a9f2b70fa4eb1
Sameer Khan <sameer_khan@student.uml.edu>
2018-07-05 16:28:49 -0400

    Added login/logout functionality with strapi

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import Strapi from 'strapi-sdk-javascript/build/main'
+import Strapi from 'strapi-sdk-javascript/build/main';
 
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
---
+++ client/store/auth.js
@@ -0,0 +1,18 @@
+import Cookies from 'js-cookie';
+
+export const state = () => {};
+
+export const mutations = {
+  setUser(state, user) {
+    state.user = user;
+    Cookies.set('user', user);
+  },
+  logout(state) {
+    state.user = null;
+    Cookies.set('user', null);
+  },
+};
+
+export const getters = {
+  username: state => state.user && state.user.username,
+};
